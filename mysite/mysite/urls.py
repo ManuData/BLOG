60a2b33f6be32516f2877a840518dce2e0233940
manuData <manuelvicarioperez@MacBook-Pro.local>
2021-08-18 01:41:58 +0200

    test django correcto

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #url(r'^test/',include('blog.urls'))
+    path('test/',include('blog.urls'))
 ]
