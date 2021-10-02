315c97ba97327056a8545cd175aa6eb5e973d27a
manuData <manuelvicarioperez@MacBook-Pro.local>
2021-10-02 18:27:20 +0200

    home responsive ok

--- mysite/static/scripts/menu_hamburguesa.js
+++ mysite/static/scripts/menu_hamburguesa.js
@@ -1,11 +1,11 @@
 
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
 
-toggleButton.addEventListener('click',() => {
+
+    
+/*const toggleButton = document.getElementsByClassName('toggle-button')[0]
+    const navbarLinks = document.getElementsByClassName('nav-links')
+
+    toggleButton.addEventListener('click',() => {
     navbarLinks.classList.toogle('active')
-})
-
-
-
+    }) */
