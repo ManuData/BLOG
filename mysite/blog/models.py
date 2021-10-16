8a6d4f1ec60b95ecf71dffdb05c576c33205ff9e
manuData <manuelvicarioperez@MacBook-Pro.local>
2021-10-16 20:01:19 +0200

    creado la tabla article en base de datos

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.fields import DateField, SlugField
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    # Es la dirección del artículo
+    slug = models.SlugField()
+    body = models.CharField(max_length=1500)
+    date = models.DateTimeField(auto_now_add = True)
+
+    
+
+    # Add in thumbnail later
+    # Add in author later
+
+
+
+
+
+
+
