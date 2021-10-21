acd1f105fffc39bd4f36d707f8073d3bcc5ed7e3
manuData <manuelvicarioperez@MacBook-Pro.local>
2021-10-21 22:03:23 +0200

    creado una url nueva para listar los articulos guardados en la base de datos

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     #url(r'^$',views.html_test),
     path('article_0',views.html_article_0,name='article_0'),
     path('article_1',views.html_article_1),
+    path('article_list',views.html_article_list),
 ]
