b54071131f1b4a0557aa3663629085ed6e1ee4b8
manuData <manuelvicarioperez@MacBook-Pro.local>
2021-11-16 17:28:37 +0100

    generado el base-html con css especifico y general por cada página

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def html_home(request):
-    return render(request,'home_minimal.html')
+    return render(request,'home.html')
 
 def html_contact(request):
     return render(request,'contact.html')
